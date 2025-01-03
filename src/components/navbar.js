728d31c0cb58469739a9b59741c70765184530fa
ardszsantos <lokgamer01@gmail.com>
2025-01-03 15:07:58 -0300

    restarting

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,26 +2,18 @@ import timao from '../assets/images/timao.png';
 
 const NavbarComponent = () => {
   return (
-    <header className="items-center w-full h-16 bg-navBack">
-      <div className="pl-6 pr-6 2xl:pl-40 2xl:pr-40 flex justify-between">
+    <header className="flex justify-center gap-2 tems-center w-full h-16 bg-navBack">
         <div className='flex gap-8 items-center'>
           <img className='' src={timao} alt="" />
-          <h1 className='jet-font text-[10px] sm:text-[22px] md:text-[24px] lg:text-[26px] xl:text-[29px] text-white'>
-            TIMÃO | STATS
-          </h1>
         </div>
-        <nav className='text-white text-[10px] sm:text-[20px] md:text-[22px] lg:text-[25px] jet-font flex items-center gap-2 2xl:gap-20'>
-          <a href="#noticias">
+        <nav className='text-white text-[10px] sm:text-[20px] md:text-[22px] lg:text-[25px] jet-font flex items-center align-top gap-4'>
+          <a href="#noticias" className='hover:text-red-700'>
             <p>NOTÍCIAS</p>
           </a>
-          <a href="#partidas">
+          <a href="#partidas" className='hover:text-red-700'>
             <p>PARTIDAS</p>
           </a>
-          <a href="#estatisticas">
-            <p>ESTATÍSTICAS</p>
-          </a>
         </nav>
-      </div>
     </header>
   );
 }
