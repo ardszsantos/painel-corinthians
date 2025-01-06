3c63db7622b34b9f49db6deaaec742749ab2177d
ardszsantos <lokgamer01@gmail.com>
2025-01-06 12:00:26 -0300

    fix for deploy

---
+++ src/app/api/news/route.ts
@@ -0,0 +1,30 @@
+// src/app/api/news/route.ts
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const q = searchParams.get('q');
+  const language = searchParams.get('language');
+  const from = searchParams.get('from');
+  const sortBy = searchParams.get('sortBy');
+
+  const NEWSAPI_KEY = 'e571451976e14d369ee53f771ad58bca';
+  const endpoint = 'https://newsapi.org/v2/everything';
+
+  try {
+    const response = await axios.get(endpoint, {
+      params: {
+        q,
+        language,
+        from,
+        sortBy,
+        apiKey: NEWSAPI_KEY,
+      },
+    });
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    return NextResponse.json({ error: 'Error fetching news' }, { status: 500 });
+  }
+}
