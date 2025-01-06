5b3858c2ab5a99386ebfb4895f123986d3cb1f5d
ardszsantos <lokgamer01@gmail.com>
2025-01-06 11:49:12 -0300

    cool hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import NewsLoader from "./components/loadNews";
 export default function Home() {
   return (
     <div className="min-h-screen p-4 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="flex justify-center">
+      <div className="relative flex justify-center group">
         <Image
           src="/timãoLogo.png"
           alt="Next.js logo"
@@ -12,6 +12,9 @@ export default function Home() {
           height={280}
           priority
         />
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-sm p-2 rounded opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+          Loading news from the previous 5 days.
+        </div>
       </div>
       <div className="mt-16">
         <NewsLoader />
