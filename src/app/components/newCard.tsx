8985aafb2c6ecde539427f4801109c344085b486
ardszsantos <lokgamer01@gmail.com>
2025-02-09 11:58:55 -0300

    wrapped the content in an A tag with the href pointing to the post's url

--- src/app/components/newCard.tsx
+++ src/app/components/newCard.tsx
@@ -6,17 +6,20 @@ interface CardProps {
     author: string;
     date: string;
     content: string;
+    url: string;
 }
 
-const NewCard: React.FC<CardProps> = ({ img, title, author, date, content }) => {
+const NewCard: React.FC<CardProps> = ({ img, title, author, date, content, url }) => {
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg  hover:scale-110 transition-all">
-            <img className="w-full h-[200px] " src={img} alt={title} />
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{title}</div>
-                <p className="text-gray-700 text-base pb-2">Por {author} em {date}</p>
-                <p className="text-white text-base">{content}</p>
-            </div>
+            <a href={url} target='_blank' rel='noreferrer'>
+                <img className="w-full h-[200px] " src={img} alt={title} />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{title}</div>
+                    <p className="text-gray-700 text-base pb-2">Por {author} em {date}</p>
+                    <p className="text-white text-base">{content}</p>
+                </div>
+            </a>
         </div>
     );
 };
